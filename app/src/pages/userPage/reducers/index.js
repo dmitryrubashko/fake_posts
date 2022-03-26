50e10974c7976d34864b64c1af890f5e32bc4638
Dzmitry Rubashka <dm.rubashko@gmail.com>
2022-03-26 03:38:59 +0300

    Removed the .idea folder

---
+++ app/src/pages/userPage/reducers/index.js
@@ -0,0 +1,43 @@
+import {
+  GET_POSTS,
+  POSTS_ERROR,
+  GET_USERS,
+  USERS_ERROR,
+} from "../../../shared/types";
+
+const initialState = {
+  posts: [],
+  isLoadingPosts: true,
+  users: [],
+  isLoadingUsers: true,
+};
+
+const UserPageReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        isLoadingPosts: false,
+      };
+    case POSTS_ERROR:
+      return {
+        isLoadingPosts: false,
+        postsError: action.payload,
+      };
+    case GET_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        isLoadingUsers: false,
+      };
+    case USERS_ERROR:
+      return {
+        isLoadingUsers: false,
+        usersError: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+export default UserPageReducer;
