c439ffaa0c8fdb98fd1e58dbeb15c07109245dd4
Dzmitry Rubashka <dm.rubashko@gmail.com>
2022-05-23 00:58:35 +0300

    Minor fixes

--- src/pages/signupPage/components/SignupPageLayout/index.js
+++ src/pages/signupPage/components/SignupPageLayout/index.js
@@ -1,5 +1,6 @@
 import { useHistory } from "react-router-dom";
 import { Formik, Field, Form } from "formik";
+import bcrypt from "bcryptjs";
 import ReactTooltip from "react-tooltip";
 import * as Yup from "yup";
 
@@ -9,11 +10,26 @@ import styles from "./styles.module.scss";
 
 const SignupPageLayout = ({ handleActivateButton }) => {
   const signupSchema = Yup.object().shape({
-    email: Yup.string().email("Invalid email").required("Required"),
+    name: Yup.string()
+      .max(50, "Name is too long - should be 50 chars maximum.")
+      .required("Required"),
+    username: Yup.string()
+      .max(50, "Username is too long - should be 50 chars maximum.")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required").max(100),
+    address: Yup.string().required(
+      'Use this example to create correct address: {"street":"Kulas Light","suite":"Apt. 556","city":"Gwenborough","zipcode":"92998-3874","geo":{"lat":"-37.3159","lng":"81.1496"}}'
+    ),
+    phone: Yup.string()
+      .max(50, "Phone number is too long - should be 50 chars maximum.")
+      .matches(/[0-9]/, "Phone can only contain numbers.")
+      .required("Required"),
+    website: Yup.string().required("Required").max(50),
+    companies: Yup.string().required("Required"),
     password: Yup.string()
       .required("No password provided.")
       .min(8, "Password is too short - should be 8 chars minimum.")
-      .max(30, "Password is too long - should be 30 chars maximum.")
+      .max(50, "Password is too long - should be 50 chars maximum.")
       .matches(/[0-9a-zA-Z]/, "Password can only contain Latin letters."),
     confirmPassword: Yup.string()
       .required("No password provided.")
@@ -34,18 +50,92 @@ const SignupPageLayout = ({ handleActivateButton }) => {
         <div className={styles.SignupPageLayout__title}>Sign Up</div>
         <Formik
           initialValues={{
+            name: "",
+            username: "",
             email: "",
+            address: "",
+            phone: "",
+            website: "",
+            companies: "",
             password: "",
             confirmPassword: "",
           }}
           validationSchema={signupSchema}
           onSubmit={(values) => {
-            console.log(values);
+            console.log(values.email);
+            fetch(`http://localhost:8080/users`, {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                name: values.name,
+                username: values.username,
+                email: values.email,
+                phone: values.phone,
+                website: values.website,
+                address: JSON.parse(values.address),
+                password: bcrypt.hashSync(values.password),
+                companies: JSON.parse(values.companies),
+              }),
+            }).then((response) => {
+              console.log("response", response); //
+              if (response.status === 204) {
+                return new Promise((resolve) => resolve(null));
+              }
+              if (!response.ok) {
+                throw new Error(response.statusText);
+              }
+              return response.json();
+            });
             history.push("/");
           }}
         >
           {({ errors, touched }) => (
             <Form noValidate>
+              <label>
+                <div className={styles.SignupPageLayout__label}>Name</div>
+                <Field
+                  className={styles.SignupPageLayout__inputField_selected}
+                  name="name"
+                  type="text"
+                  placeholder="Enter your name"
+                />
+              </label>
+              {errors.name && touched.name ? (
+                <div className={styles.SignupPageLayout__errorMessage_hidden}>
+                  {errors.name}
+                </div>
+              ) : null}
+              <ReactTooltip />
+              <label>
+                <div className={styles.SignupPageLayout__label}>Username</div>
+                <Field
+                  className={styles.SignupPageLayout__inputField_selected}
+                  name="username"
+                  type="text"
+                  placeholder="Enter your username"
+                />
+              </label>
+              <span
+                data-tip="Enter a unique username"
+                className={styles.SignupPageLayout__exclamationText}
+              >
+                {
+                  <img
+                    src={exclamation}
+                    alt={"exclamation"}
+                    className={styles.SignupPageLayout__exclamationImage}
+                  />
+                }
+              </span>
+              {errors.username && touched.username ? (
+                <div className={styles.SignupPageLayout__errorMessage_hidden}>
+                  {errors.username}
+                </div>
+              ) : null}
+              <ReactTooltip />
               <label>
                 <div className={styles.SignupPageLayout__label}>Email</div>
                 <Field
@@ -73,6 +163,68 @@ const SignupPageLayout = ({ handleActivateButton }) => {
                 </div>
               ) : null}
               <ReactTooltip />
+              <label>
+                <div className={styles.SignupPageLayout__label}>Address</div>
+                <Field
+                  className={styles.SignupPageLayout__inputField_selected}
+                  name="address"
+                  type="text"
+                  placeholder="Address"
+                />
+              </label>
+              {errors.address && touched.address ? (
+                <div className={styles.SignupPageLayout__errorMessage_hidden}>
+                  {errors.address}
+                </div>
+              ) : null}
+              <ReactTooltip />
+              <label>
+                <div className={styles.SignupPageLayout__label}>Phone</div>
+                <Field
+                  className={styles.SignupPageLayout__inputField_selected}
+                  name="phone"
+                  type="tel"
+                  placeholder="Enter your phone"
+                />
+              </label>
+              {errors.phone && touched.phone ? (
+                <div className={styles.SignupPageLayout__errorMessage_hidden}>
+                  {errors.phone}
+                </div>
+              ) : null}
+              <ReactTooltip />
+              <label>
+                <div className={styles.SignupPageLayout__label}>Website</div>
+                <Field
+                  className={styles.SignupPageLayout__inputField_selected}
+                  name="website"
+                  type="text"
+                  placeholder="Enter your website"
+                />
+              </label>
+              {errors.website && touched.website ? (
+                <div className={styles.SignupPageLayout__errorMessage_hidden}>
+                  {errors.website}
+                </div>
+              ) : null}
+              <ReactTooltip />
+              {/**/}
+              <label>
+                <div className={styles.SignupPageLayout__label}>Companies</div>
+                <Field
+                  className={styles.SignupPageLayout__inputField_selected}
+                  name="companies"
+                  type="text"
+                  placeholder="Companies"
+                />
+              </label>
+              {errors.companies && touched.companies ? (
+                <div className={styles.SignupPageLayout__errorMessage_hidden}>
+                  {errors.companies}
+                </div>
+              ) : null}
+              <ReactTooltip />
+              {/**/}
               <label>
                 <div className={styles.SignupPageLayout__label}>Password</div>
                 <Field
